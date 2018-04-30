0a3fd4ce5278bf288de9c433972ac5a3c47efd5b
Sumit Ghosh <sumit@sumit-ghosh.com>
2018-04-30 19:56:25 +0530

    Load and show an image

--- ruler.py
+++ ruler.py
@@ -1,3 +1,7 @@
+'''
+Implementing the following tutorial from pyimagesearch.com.
+https://www.pyimagesearch.com/2016/03/28/measuring-size-of-objects-in-an-image-with-opencv/
+'''
 from scipy.spatial import distance
 from imutils import perspective
 from imutils import contours
@@ -12,10 +16,15 @@ def midpoint(ptA, ptB):
 
 
 def main():
-    parser = argparse.ArgumentParser
-    parser.add_argument('-i', '--image', required=True, help='Path to input image')
-    parser.add_argument('-w', '--width', required=True, help='Width of left-most object in the image')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--image', required=True, type=str, help='Path to input image')
+    parser.add_argument('-w', '--width', type=int, help='Width of left-most object in the image')
     args = parser.parse_args()
+
+    img = cv.imread(args.image, cv.IMREAD_COLOR)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    cv.imshow('Image', gray)
+    cv.waitKey(0)
 
 
 
